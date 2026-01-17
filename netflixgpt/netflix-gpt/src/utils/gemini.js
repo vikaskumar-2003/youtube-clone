09e08b15a306d4338b5aa12b4da8aa285459a7b0
vikaskumar-2003 <vikas0352004@gmail.com>
2026-01-17 18:35:38 +0530

    Delete netflixgpt/netflix-gpt directory

--- netflixgpt/netflix-gpt/src/utils/gemini.js
+++
@@ -1,8 +0,0 @@
-import { GoogleGenAI } from "@google/genai";
-
-const gemini = new GoogleGenAI({
-    apiKey: process.env.REACT_APP_GEMINI_KEY,
-    dangerouslyAllowBrowser:true
-});
-
-export default gemini
